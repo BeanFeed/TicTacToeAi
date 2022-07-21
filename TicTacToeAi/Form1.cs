d18fd8d88bba6341b5218528733e6413b7f27abb
CrckedLua <two.austin.two@gmail.com>
2022-07-20 22:56:11 -0400

    update check win

--- TicTacToeAi/Form1.cs
+++ TicTacToeAi/Form1.cs
@@ -9,11 +9,15 @@ namespace TicTacToeAi
         }
         private bool CheckBotWin()
         {
-
-        }
-        private void Form1_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine(e);
+            if ((grid[0] + grid[1] + grid[2]) == 6) { return true; }
+            if ((grid[3] + grid[4] + grid[5]) == 6) { return true; }
+            if ((grid[6] + grid[7] + grid[8]) == 6) { return true; }
+            if ((grid[0] + grid[3] + grid[6]) == 6) { return true; }
+            if ((grid[1] + grid[4] + grid[7]) == 6) { return true; }
+            if ((grid[2] + grid[5] + grid[8]) == 6) { return true; }
+            if ((grid[0] + grid[4] + grid[8]) == 6) { return true; }
+            if ((grid[2] + grid[4] + grid[6]) == 6) { return true; }
+            return false;
         }
 
         private void slotClick(object sender, EventArgs e)
